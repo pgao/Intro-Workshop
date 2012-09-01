f6fa55bc9a92754ec9ac13911a076be781a7303a
Vaishaal <vaishaal@berkeley.edu>
2012-09-01 03:08:08 -0700

    added control panel for socket io testing

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ var app = require('http').createServer(handler)
 app.listen(8000);
 var oneSocket = null
 function handler (req, res) {
-  fs.readFile(__dirname + '/view.html',
+    console.log(req.url);
+  if (req.url == '/') {
+      console.log("IM NOW HERE");
+      file = '/view.html'
+  } else { 
+      console.log("IM HERE");
+      file = '/control.html'
+  }
+
+  fs.readFile(__dirname + file,
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -14,6 +23,12 @@ function handler (req, res) {
 
     res.writeHead(200);
     res.end(data);
+  });
+  io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('colorchange', function (data) {
+      io.sockets.emit(data['color'],data['sign']);
+  });
   });
 }
 
