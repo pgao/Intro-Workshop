6746aff21ecbf1f37ce9e779af44cf739bfb0c3e
Johan Brook <johan.jvb@gmail.com>
2012-09-03 21:52:49 -0700

    FINAL SETUP

--- software/server.js
+++ software/server.js
@@ -1,32 +1,9 @@
-var Serial = require('serialport'),
-	express = require('express');
-
+var express = require('express');
 var app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
-	
-var port = "/dev/ttyUSB0",
-	myName = "nelson";
+	server = require('http').createServer(app)
 
 app.use('/', express.static(__dirname));
 
-io.sockets.on('connection', function (socket) {
-	socket.emit('welcome', {name: myName});
-	
-	socket.on('colorchange', function (data) {
-		io.sockets.emit(data['color'],data['sign']);
-	});
-});
 
-var Port = new Serial.SerialPort(port, {
-	parser: Serial.parsers.readline('\n')
-});
-
-Port.on("data", function(data) {
-    if (!isNaN(data)) {
-        var value = parseInt(data);
-	    io.sockets.emit("red", value);
-    };
-});
 
 server.listen(8000);
